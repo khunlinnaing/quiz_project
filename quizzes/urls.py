e03f5c2fc37b6e24d73d7232a2c9335207703cc9
khunthanbwe19 <khunthanbwe1998@gmail.com>
2025-04-22 11:46:57 +0630

    'update'

--- quizzes/urls.py
+++ quizzes/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('dashboard/', views.user_dashboard, name='user_dashboard'),
     # Admin URLs
     path('admin_dashboard/', views.admin_dashboard, name='admin_dashboard'),
-    path('admin_dashboard/add-quiz/', views.add_quiz, name='add_quiz'),m
+    path('admin_dashboard/add-quiz/', views.add_quiz, name='add_quiz'),
     path('admin_dashboard/edit-quiz/<int:quiz_id>/', views.edit_quiz, name='edit_quiz'),
     path('admin_dashboard/delete-quiz/<int:quiz_id>/', views.delete_quiz, name='delete_quiz'),
     path('admin_dashboard/add-question/', views.add_question, name='add_question'),
